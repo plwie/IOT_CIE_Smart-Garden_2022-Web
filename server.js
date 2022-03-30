2eddf0ab9eb51e4e15d3507a238f43c36d73a3d1
plwie <58283153+plwie@users.noreply.github.com>
2022-03-30 16:35:47 +0700

    add db gateway

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
 var corsOption = {
     origin: "http://localhost:8081"
 };
@@ -11,3 +16,4 @@ app.use(express.urlencoded({ extended: true}));
 app.get("/", (req, res) => {
     res.json({ message: "connection successfully"});
 });
+
